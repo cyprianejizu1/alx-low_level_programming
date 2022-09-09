8b4fb6bf3357f070fc30c03cdcabb4046a2fb7f7
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-09 13:48:44 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,14 +5,14 @@
 /**
 * main - this will assign random numbers
 *
-* Return: always 0
+* Return:always 0
 *
 */
 int main(void)
 {
 	int n;
 
-        srand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
