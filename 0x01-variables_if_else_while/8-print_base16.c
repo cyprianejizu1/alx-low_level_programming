51eae3543b223a027f4f56c6c07a2a15283b0f4e
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-09 15:40:35 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
@@ -10,7 +11,7 @@ int main(void)
 	unsigned char a = '0';
 	int i;
 
-for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(a);
 		a++;
