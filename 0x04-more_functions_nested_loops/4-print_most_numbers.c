f1af51621703d2171a5e6cadc84331ace49adc6e
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-16 16:17:33 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,9 @@ void print_most_numbers(void)
 	int c;
 
 	for (c = 0; c <= 9; c++)
-	{
 		if (!(c == '2' || c == '4'))
-		       continue;	
-		_putchar(c);
-	}
+		       continue;
+	_putchar(c);
+	
 	_putchar('\n');
 }
