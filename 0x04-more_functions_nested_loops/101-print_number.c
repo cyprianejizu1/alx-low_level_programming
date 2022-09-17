fcecf0e9953e772c65df5919da7f8e024eb642d4
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-17 00:20:31 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,42 +18,28 @@ void print_number(int n)
 
 void print_number(int n)
 {
-	char ld, cr;
-	int r;
-	int ct = 0;
+	unsigned int d = 10
 
-	if (n < 0)
+	if (n < d)
 	{
-		_putchar ('-');
-		ld = (char)('0' - (n % 10));
-		n = n / -10;
+		_putchar('0' + n);
 	}
-
-		else
+	else
+	{
+		while (n >= d)
 		{
-			ld = (char)((n % 10) + '0');
-		n = n / 10;
+			d *= 10;
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
 	}
-
-	r = 0;
-	while (n > 0)
-	{
-		r = r * 10 + (n % 10);
-		n = n / 10;
-		ct++;
-	}
-
-	while (r > 0)
-	{
-		cr = (char)((r % 10) + '0');
-		_putchar(cr);
-		r = r / 10;
-		ct--;
-	}
-	while (ct != 0)
-	{
-		_putchar('0');
-			ct--;
-	}
-	_putchar(ld);
 }
