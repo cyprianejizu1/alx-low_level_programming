61fb17376afea9b993864b4418229fd2c977cf6d
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-20 16:42:05 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,3 +7,17 @@
  * Return: zero
  */
 
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
