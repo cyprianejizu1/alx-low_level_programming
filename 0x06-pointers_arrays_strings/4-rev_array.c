19a0273cfc25a101d9843fc8518ddffb9e1f627c
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-22 06:18:38 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,9 @@
+#include in.h"
+
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
+ */
+
