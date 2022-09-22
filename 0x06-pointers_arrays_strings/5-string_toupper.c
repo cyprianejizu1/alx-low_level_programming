12786976b9e962bf6edaacc9743f0fbda13de618
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-22 14:23:49 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,3 +8,17 @@
  * Return: A pointer to the changed string.
  */
 
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
