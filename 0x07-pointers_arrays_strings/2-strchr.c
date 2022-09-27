7d694297277df4f3da9c0840f0ece363071db3fe
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-26 18:48:47 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
 *@s: Pointer to the string
 *@c: Character to locate in the string
 *
-*Return: Pointer to the first occurance of the c if it's there or NULL otherwise
+* Return: Pointer to the first occurance of the c if it's there or NULL otherwise
 */
 
 char *_strchr(char *s, char c)
