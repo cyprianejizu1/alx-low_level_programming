da95e842aced04801ab62b21d91036b9078940ad
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-26 17:27:24 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - main function.
+ * @s: Pointer to the string to search.
+ * @accept: Pointer to set of bytes to find.
+ *
+ * Description: This function searches a string for any of a set of bytes.
+ *
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found.
+ */
