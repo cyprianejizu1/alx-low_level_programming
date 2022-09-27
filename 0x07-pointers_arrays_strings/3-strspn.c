e2a961fa63ddf80119025fb5c98d975949f24c1f
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-26 17:20:47 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strspn - main function.
+ * @s: Pointer to the string.
+ * @accept: Pointer to the character to print.
+ *
+ * Description: This function gets the length of a prefix substring.
+ *
+ * Return:  the number of bytes in the initial segment of *s which
+ * consist only of bytes from *accept.
+ */
