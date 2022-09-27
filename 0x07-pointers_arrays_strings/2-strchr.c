bd544eafededb00b522df4163c1873bcff18330d
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-09-26 17:03:31 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,12 @@
 *
 *Description: This function locates a character in a string
 *
-*Return: A pointer to the first occurance of the character if it's there or NULL otherwise
+*Return: Pointer to the first occurance of the c if it's there or NULL otherwise
 */
 
 char *_strchr(char *s, char c)
 {
-	do{
+	do {
 		if (*s == c)
 		{
 			return (s);
