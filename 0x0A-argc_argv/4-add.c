a351234d941596404c2b5f6ad16d53671e11658a
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-03 15:21:27 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* num_checker - checks if a given char is number or not
+* @a: char to checked
+* Return: 1, if its a number, else 0
+**/
+
+int num_checker(char *a)
