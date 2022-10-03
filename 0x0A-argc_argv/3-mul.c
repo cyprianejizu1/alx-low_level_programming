58378b5aa7703a01fc1a9d945a543c904ae39d38
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-03 03:18:24 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two number
+ * @argc: arguement count
+ * @argc: array of pointers to arguement string
+ * Return: result of multipilication or 1
+ **/
+
+main(int argc, char *argv[])
+{
+
+
+}
