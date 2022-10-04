c2b435dc1e0cfb7501ade31fd14fc4e478e90dfc
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-03 18:39:54 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,15 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int index;
 
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
 
 }
