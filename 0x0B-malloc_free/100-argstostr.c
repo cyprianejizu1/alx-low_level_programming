6e36f56d7eb4ce59d7ff9cb79410235b0327190d
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-04 15:57:16 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,3 +27,21 @@ char *argstostr(int ac, char **av)
 
 	str = malloc(sizeof(char) * size + 1);
 
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+	str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
+}
