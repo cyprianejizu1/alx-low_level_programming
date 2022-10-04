36af65a090103c15b17c19d93fc08a47ad4158dd
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-03 20:04:42 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,3 +3,15 @@
 
 /**
 * str_concat - concatenates two strings.
+* @s1: The string to be concatenated upon.
+* @s2: The string to be concatenated to s1.
+*
+* Return: pointer the newly allocated space in memrory containing the concatenated strings.
+**/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat_str;
+	int index, concat_index = 0, size = 0;
+
+}
