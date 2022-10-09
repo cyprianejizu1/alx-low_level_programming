64ad00b2aa042b977f5fe2f5171f35f04685aac1
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-09 06:43:57 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,3 +17,35 @@ int _isNum(char *num)
 	}
 	return (1);
 }
+
+/**
+ * *_memset - copies a character to the first n characters of the string pointer
+ * @s: original string
+ * @b: value to replace
+ * @n: number of bytes
+ * Return: s (string modify)
+ **/
+
+char *_memset(char *s, char b; unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+	return (s);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer of character
+ * Return: the length of a string
+ **/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while ((*s + len) != '\0')
+		len++;
+	return (len);
+}
