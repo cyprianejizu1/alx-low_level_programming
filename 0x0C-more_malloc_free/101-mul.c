905222428dd281fcc6392102e7581c4e73d5213c
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-09 17:24:46 -0700

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,101 +1,98 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * _isNum - check if is a number
- * @num: string to check
- * Return: 1 is num, 0 not num
- **/
-
+* _isNum - check if is a number
+*@num: string to check
+*Return: 1 is numm, 0 not num
+*/
 int _isNum(char *num)
 {
-	int a;
+	int i;
 
-	for (a = 0; num[a] != '\0'; a++)
+	for (i = 0; num[i] != '\0'; i++)
 	{
-		if (num[a] < '0' || num[a] > '9')
+		if (num[i] < '0' || num[i] > '9')
 			return (0);
 	}
 	return (1);
 }
 
 /**
- * *_memset - copies a character to the first n characters
- * @s: original string
- * @b: value to replace
- * @n: number of bytes
- * Return: s (string modify)
- **/
-
+* *_memset - copies a character to the firstn characters of the string pointed
+*@s: original string
+*@b: value to remplace
+*@n: number of bytes
+*Return: s (string modify)
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a;
+	unsigned int i;
 
-	for (a = 0; a < n; a++)
-		s[a] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: pointer of character
- * Return: the length of a string
- **/
-
+* _strlen - returns the lenght of a string
+*@s: poiter of character
+*Return: the length of a string
+*/
 int _strlen(char *s)
 {
-	int len = 0;
+	int len;
 
-	while ((*s + len) != '\0')
+	len = 0;
+	while (*(s + len) != '\0')
 		len++;
 	return (len);
 }
 
 /**
- * main - multiples 2 positive numbers
- * @argc: argument counter
- * @argv: number to multiply
- * Return: 0
- **/
-
+* main - multiple 2 positive numbers
+*@argc: argument counter
+*@argv: number to multiply
+*Return: 0 (success)
+*/
 int main(int argc, char *argv[])
 {
-	int length, prod, index, digit, a1, a2, c;
-	int *next;
+	int length, c, prod, i, j, l1, l2;
+	int *res;
 
 	if ((argc != 3 || !(_isNum(argv[1]))) || !(_isNum(argv[2])))
-		puts("Error");
-	exit(98);
-	a1 = _strlen(argv[1]), a2 = _strlen(argv[2]);
-	length = a1 + a2;
-	next = calloc(length, sizeof(int *));
-	if (next == NULL)
-		puts("Error");
-	exit(98);
-	for (index = a2 - 1; index > -1; index--)
+		puts("Error"), exit(98);
+	l1 = _strlen(argv[1]), l2 = _strlen(argv[2]);
+	length = l1 + l2;
+	res = calloc(length, sizeof(int *));
+	if (res == NULL)
+		puts("Error"), exit(98);
+	for (i = l2 - 1; i > -1; i--)
 	{
 		c = 0;
-		for (digit = a1; digit > -1; digit--)
+		for (j = l1; j > -1; j--)
 		{
-			prod = (argv[2][index] - '0') * (argv[1][digit] - '0');
+			prod = (argv[2][i] - '0') * (argv[1][j] - '0');
 			c = (prod / 10);
-			next[(index + digit) + 1] += (prod % 10);
-			if (next[(index + digit) + 1] > 9)
+			res[(i + j) + 1] += (prod % 10);
+			if (res[(i + j) + 1] > 9)
 			{
-				next[index + digit] += next[(index + digit) + 1] / 10;
-
+				res[i + j] += res[(i + j) + 1] / 10;
+				res[(i + j) + 1] = res[(i + j) + 1] % 10;
 			}
-			next[(index + digit) + 1] += c;
+			res[(i + j) + 1] += c;
 		}
 	}
 
-	if (next[0] == 0)
-		index = 1;
+	if (res[0] == 0)
+		i = 1;
 	else
-		index = 0;
-	for (; index < length; index++)
-		printf("%d", next[index]);
+		i = 0;
+	for (; i < length; i++)
+		printf("%d", res[i]);
 
 	printf("\n");
-	free(next);
+	free(res);
 	return (0);
 }
