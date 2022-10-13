7fec85d04a7e0b8d19447a5333e40ba8c7fd5145
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-12 19:58:27 -0700

    function that executes a function given as parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator -
+ *
+ * @size: the size of the array
+ * @action: a pointer to the function
+ *
+ * Return: Nothing
+ **/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
