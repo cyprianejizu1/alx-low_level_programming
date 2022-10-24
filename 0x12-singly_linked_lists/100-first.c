5a62a2bcfe6c6d527bd8ce109fae2d7465ab5916
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-23 17:53:43 -0700

    function that prints before amain function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "list.h"
+
+/**
+ * first - print a statement before the main
+ * function is executed
+ **/
+
+void main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
