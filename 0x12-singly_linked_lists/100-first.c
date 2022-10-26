226fcf0e923905edda15862917b5f8d1e16b10b2
cyprianejizu1 <ekemsinternetbiz@gmail.com>
2022-10-25 17:07:41 -0700

    function that prints before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void first(void) __attribute__ ((constructor));
  * function is executed
  **/
 
-void main(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
